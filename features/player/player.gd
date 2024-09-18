08d277a567bb4b2a75b142f2ebdcc26f3e1982e0
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2024-09-18 22:36:50 +0200

    Add character rotation

--- features/player/player.gd
+++ features/player/player.gd
@@ -2,8 +2,8 @@ class_name Player extends CharacterBody3D
 
 
 const SPEED : float = 1.0
-const ACCELERATION : float = 1.0
-const DECELERATION : float = 0.25
+const ACCELERATION : float = 0.5
+const DECELERATION : float = 0.1
 
 
 var input_dir : Vector2 = Vector2.ZERO
@@ -12,9 +12,11 @@ var inspecting : bool = false
 var moving_to_inspectable : bool = false
 var freeroaming : bool = false
 var movement_disabled : bool = true
+var blend_pos_remapped : float = 0.0
 
 
 @onready var animations : AnimationPlayer = $Character.get_node("%Animations")
+@onready var animation_tree : AnimationTree = $Character.get_node("%AnimationTree")
 
 
 func _ready() -> void:
@@ -34,13 +36,15 @@ func _physics_process(delta: float) -> void:
 func _process(_delta: float) -> void:
 	DebugPanel.add_property("global_pos", global_position, 0)
 
+	animation_tree.set("parameters/blend_position", velocity.length())
+
 
 func update_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 
-func update_input(_delta: float) -> void:
+func update_input(delta: float) -> void:
 	if input_dir:
 		velocity.x = lerpf(velocity.x, input_dir.x * SPEED, ACCELERATION)
 		velocity.z = lerpf(velocity.z, input_dir.y * SPEED, ACCELERATION)
@@ -51,6 +55,14 @@ func update_input(_delta: float) -> void:
 			if distance_to_click < 0.01:
 				input_dir = Vector2.ZERO
 				freeroaming = false
+
+		var left_axis : Vector3 = Vector3.UP.cross(Vector3(input_dir.x, 0.0, input_dir.y))
+		var rotation_basis : Quaternion = Basis(left_axis, Vector3.DOWN, \
+		Vector3(input_dir.x, 0.0, input_dir.y)).get_rotation_quaternion()
+		var model_scale : Vector3 = $Character.basis.get_scale()
+		$Character.basis = Basis($Character.basis.get_rotation_quaternion().slerp( \
+		rotation_basis, delta * 10.0)).scaled(model_scale)
+
 
 	else:
 		velocity.x = move_toward(velocity.x, 0, DECELERATION)
@@ -72,3 +84,5 @@ func on_animation_finished(anim_name: StringName) -> void:
 
 	movement_disabled = false
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	animation_tree.active = true
+	animations.active = false
